11985e15673851f77a2fa64a6eb6355ef5b0f2e6
siraniks <nashayexa@gmail.com>
2017-05-20 02:50:12 +0800

    fix index.php
    - remove announcement slider 
    - enclosed the_category to .card-block
    - added default to labels for Read Buttons 
    - remove admin section

--- index.php
+++ index.php
@@ -11,7 +11,6 @@
 get_header('main'); ?>
 <main>
         <div class="container">
-<!--            <h1>INDEX</h1>-->
             
             <!-- alert bar
             <div class="alert alert-warning alert-dismissible fade in" role="alert">
@@ -27,16 +26,11 @@ get_header('main'); ?>
             
             <div class="row">
                 <!-- news section -->
-                <!-- Announcement Slider -->
-                <div class="col-md-12">
-                    <?php if(function_exists('dynamic_sidebar') && dynamic_sidebar("Announcement Slider")):else: ?><?php endif; ?>
-                </div>
-                <div class="col-md-9">
-                    
-
-                    
+                <div class="col-md-9">                    
                 <!-- news section -->   
-                    <h3 class="center"><?php the_category(); ?></h3>
+                    <div class="card card-block">
+                        <h3 class="aligncenter">Category: <?php the_category(); ?></h3>
+                    </div>
                     
                     <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
                     
@@ -54,17 +48,19 @@ get_header('main'); ?>
                             </div>
                             -->
                             <div class="col-sm-12">
+                                
                                 <div class="float-xs-right">
 <!--                                <php edit_post_link('<button id="editBtn" class="btn btn-primary" style="font-size: 12px;"><i class="fa fa-1x fa-fw fa-pencil-square-o" aria-hidden="true"></i> Edit This Post</button>'); ?> -->
                                 </div>
                                 
+
                                 <div id="post-<?php the_ID(); ?>" <?php post_class( 'card-block news-item-content' ); ?>>
 <!--                                    <h6 class="cat-text"><php the_category( ', ' ); ?></h6>-->
                                     <h4 class="card-title"><a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h4>
                                     <p>
                                         <?php the_excerpt(); ?>
                                     </p>
-                                    <a id="readBtn" href="<?php the_permalink(); ?>" class="btn btn-primary"><?php echo get_theme_mod( 'read-btn_textbox' ); ?></a>
+                                    <a id="readBtn" href="<?php the_permalink(); ?>" class="btn btn-primary"><?php echo get_theme_mod( 'read-btn_textbox', 'Read' ); ?></a>
                                 </div>
                             </div>
                         </div>
@@ -84,85 +80,7 @@ get_header('main'); ?>
                 <?php get_sidebar( 'index' ); ?>
                 
             </div><!-- header content -->
-        </div><!-- header content container -->
-        
-        <!-- admin section -->
-        <div class="officers hidden-sm-down" style="background-color: #eceeef;">
-            <div class="container">
-                <div class="row">
-                    <div class="card-deck-wrapper">
-                        <div class="card-deck">
-                            
-                            <div class="card">
-                                <div id="admin-sec" class="card-block">
-                                    <img src="<?php 
-                                              $dir = get_template_directory_uri(); ;
-                                              $img = "/images/adminpic.png";
-                                              echo get_theme_mod( 'admin-sec', $dir . $img ); ?>" alt="Secretary" class="center">
-                                    <!-- <img src="<php echo get_template_directory_uri(); ?>/images/adminpic.png" alt="Card image" class="center"> -->
-                                </div>
-                            </div>
-                            <div class="card">
-                                <div id="admin-rd" class="card-block">
-                                    <img src="<?php 
-                                              $dir = get_template_directory_uri(); ;
-                                              $img = "/images/adminpic2.png";
-                                              echo get_theme_mod( 'admin-rd', $dir . $img ); ?>" alt="Regional Director" class="center">
-                                    <!-- <img src="<php echo get_template_directory_uri(); ?>/images/adminpic2.png" alt="Card image" class="center"> -->
-                                </div>
-                            </div>
-                            <div class="card">
-                                <div id="admin-penro" class="card-block">
-                                    <img src="<?php 
-                                              $dir = get_template_directory_uri(); ;
-                                              $img = "/images/adminpic3.png";
-                                              echo get_theme_mod( 'admin-penro', $dir . $img ); ?>" alt="PENRO Officer" class="center">
-                                    <!-- <img src="<php echo get_template_directory_uri(); ?>/images/adminpic3.png" alt="Card image" class="center"> -->
-                                </div>
-                            </div>
-                            
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- carousel admin for mobile only -->
-        <div class="container">
-            <h3 class="center hidden-md-up">Administrators</h3>
-            <div class="mx-auto" style="background-color:blue; width:200px; margin-bottom: 10px;">
-                <div id="carousel-example-generic" class="carousel slide hidden-md-up" data-ride="carousel">
-                      <div class="carousel-inner" role="listbox">
-                            <div class="carousel-item active">
-                                <img src="<?php 
-                                  $dir = get_template_directory_uri(); ;
-                                  $img = "/images/adminpic.png";
-                                  echo get_theme_mod( 'admin-sec', $dir . $img ); ?>" alt="Secretary">
-                            </div>
-                            <div class="carousel-item">
-                                <img src="<?php 
-                                  $dir = get_template_directory_uri(); ;
-                                  $img = "/images/adminpic2.png";
-                                  echo get_theme_mod( 'admin-rd', $dir . $img ); ?>" alt="Regional Director">
-                            </div>
-                          <div class="carousel-item">
-                              <img src="<?php 
-                                  $dir = get_template_directory_uri(); ;
-                                  $img = "/images/adminpic3.png";
-                                  echo get_theme_mod( 'admin-penro', $dir . $img ); ?>" alt="PENRO Officer">
-                            </div>
-                      </div>
-                      <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-                            <span class="icon-prev" aria-hidden="true"></span>
-                            <span class="sr-only">Previous</span>
-                      </a>
-                      <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-                            <span class="icon-next" aria-hidden="true"></span>
-                            <span class="sr-only">Next</span>
-                      </a>
-                </div>
-            </div>
-        </div>
-        <!-- admin section -->    
+        </div><!-- header content container -->    
         
         <div class="embed-container maps">
             
