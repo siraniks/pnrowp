7e8e6327eca7fed0df25fa944fddf126e0ee7946
siraniks <nashayexa@gmail.com>
2017-05-21 01:37:20 +0800

    fix mailto handler

--- page.php
+++ page.php
@@ -27,8 +27,23 @@ get_header('main'); ?>
                                 <!-- <img src="<?php echo get_template_directory_uri(); ?>/images/no-thumbs.png" class="img-fluid" alt="<?php the_title(); ?>"> -->
                             <?php } ?>
                             
-                            <h2 class="card-title news-title"><a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+                            <h2 class="card-title news-title"><?php the_title(); ?></h2>
                             
+                            <ul class="sharelist" style="font-size: 18px;">
+                                
+                                <li><a href="http://www.facebook.com/sharer.php?u=<?php the_permalink();?>&t=<?php the_title(); ?>" title="Share this post on Facebook" target="blank" rel="nofollow" class="icon-fb"><i class="fa fa-facebook-square fa-fw"></i></a></li>
+                                
+                                <li><a href="https://twitter.com/intent/tweet?text=<?php the_title(); ?>&source=<?php bloginfo('name'); ?>&url=<?php the_permalink();?>" title="Share this post on Twitter" class="icon-tw"><i class="fa fa-twitter fa-fw"></i></a></li>
+                                
+                                <li><a href="https://www.linkedin.com/shareArticle?mini=true&url=<?php the_permalink();?>&title=<?php the_title(); ?>&summary=<?php the_excerpt(); ?>&source=<?php bloginfo('name'); ?>-<?php bloginfo('description');  ?>" title="Share this post on LinkedIn" class="icon-lin"><i class="fa fa-linkedin fa-fw"></i></a></li>
+                                
+                                <li><a href="https://getpocket.com/save?url=<?php the_permalink();?>&title=<?php the_title(); ?>" title="Read it later on Pocket" class="icon-pckt"><i class="fa fa-get-pocket fa-fw"></i></a></li>
+                                
+                                <li><a href="javascript:window.print()" class="icon-prntr"><i class="fa fa-fw fa-print"></i></a></li>
+                                
+                                <li><a href="mailto:?subject=<?php the_title(); ?>&body=Read it from here - <?php the_permalink(); ?>" title="Email This" class="icon-mail"><i class="fa fa-fw fa-envelope"></i></a></li>
+                                
+                            </ul><br>
                             
                                 <?php the_content(); ?>
                             
