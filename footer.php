773201498e7fb63c1577cb55f73746683cebcd22
siraniks <nashayexa@gmail.com>
2017-05-21 01:35:52 +0800

    simplified separated colors into one configuration

--- footer.php
+++ footer.php
@@ -2,14 +2,35 @@
         <footer>
         <div class="footer">
             <div class="container">
+                <div class="row">
+                    <!-- Site map / expanded footer Links -->
+<!--
+                     <div class="mx-auto hidden-md-down">
+
+                        <php
+                        $footerparams = array(
+                        'menu'  =>  'Footer Menu',
+                        'container' => '',
+                        'theme_location' => 'footnav',
+                        'items_wrap' => footnav_wrap(),
+                        'depth' => '0',
+                        'walker' => new footnav_walker()
+                        );
+
+                          echo strip_tags(wp_nav_menu( $footerparams ), '<a>' ); 
+                        ?>
+
+                    </div> wrapper 
+-->
+                </div>
                 <div class="row-fluid">
-                    <div class="span12">
+                    <div class="mx-auto">
                         
                         <!-- transparency logo -->
                         <div class="col-lg-9 footer-text hidden-sm-down">
                             <h6 class="footdnr">Department of Environment and Natural Resources</h6>
                             <h4 class="footpnr"><a><?php bloginfo('name'); ?></a></h4>
-                            <h5 class="footrnr"><?php bloginfo('description'); ?></h5><br>
+                            <h5 class="footrnr"><?php bloginfo('description'); ?></h5><br class="footbr">
                             <i class="footcnr fa fa-map-marker fa-fw" aria-hidden="true" ></i>&nbsp;<span class="footcontact"><?php echo get_theme_mod('contact_adr', 'Block 1, Martinez Subd., Zone IV, 9506 City of Koronadal'); ?></span><br>
                             <i class="footcnr fa fa-envelope fa-fw" aria-hidden="true" ></i>&nbsp;<span class="footcontact"><?php echo get_theme_mod('contact_email', 'penro.southcotabato@yahoo.com'); ?></span><br>
                             <i class="footcnr fa fa-phone fa-fw" aria-hidden="true"></i>&nbsp;<span class="footcontact"><?php echo get_theme_mod('contact_telnum', '(083) 228-3502'); ?></span>
@@ -23,23 +44,7 @@
                             <!-- mobile -->
                             <a href="<?php echo esc_url( get_permalink( get_page_by_title( 'Transparency Seal' ) ) ); ?>" data-toggle="tooltip" data-placement="bottom" title="What is Transparency Seal?"><img class="transparency-seal75 hidden-sm-up" src="<?php echo get_template_directory_uri(); ?>/images/transparency-seal.png" /></a>
                         </div>
-                        <!-- Site map / expanded footer Links -->
-                        <!-- <div class="col-lg-9 hidden-md-down">
-                                
-                            <php
-                            $footerparams = array(
-                            'menu'  =>  'Footer Menu',
-                            'container' => '',
-                            'theme_location' => 'footnav',
-                            'items_wrap' => footnav_wrap(),
-                            'depth' => '0',
-                            'walker' => new footnav_walker()
-                            );
-
-                              echo strip_tags(wp_nav_menu( $footerparams ), '<a>' ); 
-                            ?>
-                                
-                        </div> wrapper -->
+                        
                     </div><!-- span12 -->
                 </div><!-- row-fluid -->
             </div><!-- container -->
@@ -75,9 +80,9 @@
                         </ul><br>
                         <div class="span12 hidden-sm-up" style="font-size: 12px;">
                             <p class="mx-auto">
-                                <i class="footcnr fa fa-map-marker fa-fw" aria-hidden="true" ></i>&nbsp;<span class="footcontact"><?php echo get_theme_mod('contact_adr', 'Blk. 1, Martinez Subd., Zone IV, 9506 Koronadal City'); ?></span><br>
-                                <i class="footcnr fa fa-envelope fa-fw" aria-hidden="true" ></i>&nbsp;<span class="footcontact"><?php echo get_theme_mod('contact_email', 'penro.southcotabato@yahoo.com'); ?></span><br>
-                                <i class="footcnr fa fa-phone fa-fw" aria-hidden="true"></i>&nbsp;<span class="footcontact"><?php echo get_theme_mod('contact_telnum', '(083) 228-3502'); ?></span>
+                                <i class="footcnr fa fa-map-marker fa-fw" aria-hidden="true" ></i>&nbsp;<span id="adr" class="footcontact"><?php echo get_theme_mod('contact_adr', 'Blk. 1, Martinez Subd., Zone IV, 9506 Koronadal City'); ?></span><br>
+                                <i class="footcnr fa fa-envelope fa-fw" aria-hidden="true" ></i>&nbsp;<span id="email" class="footcontact"><?php echo get_theme_mod('contact_email', 'penro.southcotabato@yahoo.com'); ?></span><br>
+                                <i class="footcnr fa fa-phone fa-fw" aria-hidden="true"></i>&nbsp;<span id="telnum" class="footcontact"><?php echo get_theme_mod('contact_telnum', '(083) 228-3502'); ?></span>
                             </p>
                             <p class="center">Copyright <?php echo date('Y'); ?> PENRO <?php bloginfo('description'); ?>. <br>All rights reserved</p>
                             <a id="feedbackbtn" href="#" class="aligncenter ownBtn-login-sm btn-success" data-toggle="modal" data-target="#feedbackModal" title="Send Feedback"><i class="fa fa-paper-plane-o fa-fw" aria-hidden="true"></i>&nbsp;Send Feedback</a><br>
