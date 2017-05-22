605dbe1ac9bd140ad94c2c90a773efa067cb5684
siraniks <nashayexa@gmail.com>
2017-05-22 17:36:57 +0800

    numerous changes
    - change col-xs-12 to mx-auto
    - relayout and add featured image on post
    -

--- front-page.php
+++ front-page.php
@@ -39,21 +39,32 @@ get_header('main'); ?>
                     <div class="news-item-wrapper">
                         
                         <div class="row">
-                            
-                            <div class="col-sm-12">
-                                
-                                
+                            <div class="mx-auto">
                                 <div id="post-<?php the_ID(); ?>" <?php post_class( 'card-block news-item-content' ); ?>>
                                     
-                                    
-                                    
-                                    <h6 class="cat-text"><?php the_category( ' ' ); ?></h6>
-                                    <h4 class="card-title"><a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h4>
-                                    
-                                    <?php the_excerpt(); ?>
-                                    
-                                    <a id="readBtn" href="<?php the_permalink(); ?>" class="btn btn-primary"><?php echo get_theme_mod( 'read-btn_textbox', 'Read' ); ?></a>
-                                    
+                                    <div class="col-md-4">
+                                        <div class="post-thumb-wrapper">
+                                            <?php if (has_post_thumbnail()) { ?>
+                                                <?php the_post_thumbnail( 
+                                                    'full', array( 
+                                                        'class' => 'post-thumb-img' 
+                                                    )); 
+                                                ?>
+                                            <?php } else { ?>
+                                                <img class="roundcorners" src="<?php echo get_template_directory_uri(); ?>/images/nothumbs.png" />
+                                            <?php } ?>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-8">
+                                        
+                                        <div class="post-thumb-content">
+                                            <h4 class="card-title"><a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h4>
+                                            <h6 class="cat-text"><?php the_category( ' ' ); ?></h6>
+                                            <?php the_excerpt(); ?>
+                                        </div>
+
+                                        <a id="readBtn" href="<?php the_permalink(); ?>" class="btn btn-primary"><?php echo get_theme_mod( 'read-btn_textbox', 'Read' ); ?></a>
+                                    </div>
                                 </div>
                             </div>
                             
