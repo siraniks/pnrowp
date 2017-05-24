8a45acdf6cbd7b17b75bc7ff2d3e0b612a4b00da
siraniks <nashayexa@gmail.com>
2017-05-25 01:28:04 +0800

    the search form

---
+++ searchform.php
@@ -0,0 +1,8 @@
+<form role="search" method="get" action="<?php echo home_url(); ?>">
+    <div class="input-group">
+        <input type="text" class="form-control" value name="s" placeholder="Search for...">
+        <span class="input-group-btn">
+             <button type="submit" class="btn btn-secondary searchbtn" type="button"><i class="fa fa-search" aria-hidden="true"></i></button> 
+        </span>
+    </div>
+</form>
