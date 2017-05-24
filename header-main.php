d46f50ddfb491c414095c284c00ed2f77fda9cb7
siraniks <nashayexa@gmail.com>
2017-05-25 01:27:52 +0800

    create a searchform

--- header-main.php
+++ header-main.php
@@ -187,14 +187,7 @@
                 
                 <!-- SEARCH BOX -->
                 <div id="searchbox" class="container hidden-md-down aligncenter" style="width: 65%;">
-                    <form role="search" method="get" action="<?php echo home_url(); ?>">
-                        <div class="input-group">
-                            <input type="text" class="form-control" value name="s" placeholder="Search for...">
-                            <span class="input-group-btn">
-                                 <button type="submit" class="btn btn-secondary searchbtn" type="button"><i class="fa fa-search" aria-hidden="true"></i></button> 
-                            </span>
-                        </div>
-                    </form>
+                    <?php get_search_form(); ?>
                 </div>
                 
             
