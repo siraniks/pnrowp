685487d692f2db7ea68e187f00b25e04ab372ad5
siraniks <nashayexa@gmail.com>
2017-05-26 02:08:51 +0800

    includes all wrapper of original searchbox

--- searchform.php
+++ searchform.php
@@ -1,8 +1,13 @@
-<form role="search" method="get" action="<?php echo home_url(); ?>">
-    <div class="input-group">
-        <input type="text" class="form-control" value name="s" placeholder="Search for...">
-        <span class="input-group-btn">
-             <button type="submit" class="btn btn-secondary searchbtn" type="button"><i class="fa fa-search" aria-hidden="true"></i></button> 
-        </span>
-    </div>
-</form>
+<div id="searchbox" class="container">
+    <div class="hidden-sm-up card card-block">.</div>
+    <br>
+        <form role="search" method="get" action="<?php echo home_url(); ?>">
+            <div class="input-group">
+                <input type="text" class="form-control" value name="s" placeholder="Search for...">
+                <span class="input-group-btn">
+                     <button type="submit" class="btn btn-secondary searchbtn" type="button"><i class="fa fa-search" aria-hidden="true"></i></button> 
+                </span>
+            </div>
+        </form>
+    <br>       
+</div>
