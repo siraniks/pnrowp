8e723e8bdc75f9a946a85dc9823fdc7527e97586
siraniks <nashayexa@gmail.com>
2017-05-28 22:05:59 +0800

    add .noprnt for sections that doesn't need to show on media print
    - remove mobile carouself for admins

--- front-page.php
+++ front-page.php
@@ -20,7 +20,7 @@ get_header('main'); ?>
             </div>    
         </div> 
     
-        <div class="container">
+        <div id="noprnt" class="container">
             
             <div class="row">
                 <!-- news section -->
@@ -90,7 +90,7 @@ get_header('main'); ?>
         </div><!-- header content container -->
         
         <!-- admin section -->
-        <div class="officers admin_panel-bg">
+        <div class="officers admin_panel-bg" id="noprnt">
             <div class="container">
                 <div class="row">
                     <div class="card-deck-wrapper">
@@ -142,55 +142,9 @@ get_header('main'); ?>
             </div>
         </div>
         
-        <!-- carousel admin for mobile only -->
-        
-<!--
-        <div class="container">
-            <div class="card card-block">
-                <h3 class="center admintext hidden-md-up">Administrators</h3>
-                <div class="mx-auto" style="background-color:transparent; width:200px; margin-bottom: 10px;">
-                    <div id="carousel-example-generic" class="carousel slide hidden-md-up" data-ride="carousel">
-                          <div class="carousel-inner" role="listbox">
-                                <div class="carousel-item active">
-                                    <img src="<?php 
-                                      $dir = get_template_directory_uri(); ;
-                                      $img = "/images/adminpic.png";
-                                      echo get_theme_mod( 'admin-sec', $dir . $img ); ?>" alt="Secretary">
-                                    <div class="carousel-caption">
-                                        <h3>Cimatu</h3>
-                                        <p>DENR Secretary</p>
-                                    </div>
-                                </div>
-                                <div class="carousel-item">
-                                    <img src="<?php 
-                                      $dir = get_template_directory_uri(); ;
-                                      $img = "/images/adminpic.png";
-                                      echo get_theme_mod( 'admin-rd', $dir . $img ); ?>" alt="Regional Director">
-                                </div>
-                              <div class="carousel-item">
-                                  <img src="<?php 
-                                      $dir = get_template_directory_uri(); ;
-                                      $img = "/images/adminpic.png";
-                                      echo get_theme_mod( 'admin-penro', $dir . $img ); ?>" alt="PENRO Officer">
-                                </div>
-                          </div>
-                          <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-                                <span class="icon-prev" aria-hidden="true"></span>
-                                <span class="sr-only">Previous</span>
-                          </a>
-                          <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-                                <span class="icon-next" aria-hidden="true"></span>
-                                <span class="sr-only">Next</span>
-                          </a>
-                    </div>
-                </div>
-            </div>
-        </div>
--->
-        
         <!-- admin section -->    
         
-        <div class="embed-container maps">
+        <div class="embed-container maps" id="noprnt">
             
             <?php if(function_exists('dynamic_sidebar') && dynamic_sidebar("GMap")):else: ?><?php endif; ?>
             
