d64f59bd4f193758c934a73018ced4ce3831e680
siraniks <nashayexa@gmail.com>
2017-06-01 23:09:00 +0800

    Feature: Toggleable / Disable show comments
    Tweak: add fa-1x for share list button

--- single.php
+++ single.php
@@ -36,23 +36,23 @@ get_header('main'); ?>
 
                             <ul class="sharelist" style="font-size: 18px;">
                                 
-                                <li><a href="http://www.facebook.com/sharer.php?u=<?php the_permalink();?>&t=<?php the_title(); ?>" title="Share this post on Facebook" target="blank" rel="nofollow" class="icon-fb"><i class="fa fa-facebook-square fa-fw"></i></a></li>
+                                <li><a href="http://www.facebook.com/sharer.php?u=<?php the_permalink();?>&t=<?php the_title(); ?>" title="Share this post on Facebook" target="blank" rel="nofollow" class="icon-fb"><i class="fa fa-1x fa-facebook-square fa-fw"></i></a></li>
                                 
-                                <li><a href="https://plus.google.com/share?url=<?php the_permalink(); ?>" title="Share this post on Google Plus" target="blank" rel="nofollow" class="icon-gplus"><i class="fa fa-google-plus fa-fw"></i></a></li>
+                                <li><a href="https://plus.google.com/share?url=<?php the_permalink(); ?>" title="Share this post on Google Plus" target="blank" rel="nofollow" class="icon-gplus"><i class="fa fa-1x fa-google-plus fa-fw"></i></a></li>
                                 
-                                <li><a href="https://twitter.com/intent/tweet?text=<?php the_title(); ?>&source=<?php bloginfo('name'); ?>&url=<?php the_permalink();?>" title="Share this post on Twitter" class="icon-tw"><i class="fa fa-twitter fa-fw"></i></a></li>
+                                <li><a href="https://twitter.com/intent/tweet?text=<?php the_title(); ?>&source=<?php bloginfo('name'); ?>&url=<?php the_permalink();?>" title="Share this post on Twitter" class="icon-tw"><i class="fa fa-1x fa-twitter fa-fw"></i></a></li>
                                 
-                                <li><a href="https://www.linkedin.com/shareArticle?mini=true&url=<?php the_permalink();?>&title=<?php the_title(); ?>&summary=<?php the_excerpt(); ?>&source=<?php bloginfo('name'); ?>-<?php bloginfo('description');  ?>" title="Share this post on LinkedIn" class="icon-lin"><i class="fa fa-linkedin fa-fw"></i></a></li>
+                                <li><a href="https://www.linkedin.com/shareArticle?mini=true&url=<?php the_permalink();?>&title=<?php the_title(); ?>&summary=<?php the_excerpt(); ?>&source=<?php bloginfo('name'); ?>-<?php bloginfo('description');  ?>" title="Share this post on LinkedIn" class="icon-lin"><i class="fa fa-1x fa-linkedin fa-fw"></i></a></li>
                                 
-                                <li><a href="https://getpocket.com/save?url=<?php the_permalink();?>&title=<?php the_title(); ?>" title="Read it later on Pocket" class="icon-pckt"><i class="fa fa-get-pocket fa-fw"></i></a></li>
+                                <li><a href="https://getpocket.com/save?url=<?php the_permalink();?>&title=<?php the_title(); ?>" title="Read it later on Pocket" class="icon-pckt"><i class="fa fa-1x fa-get-pocket fa-fw"></i></a></li>
                                 
-                                <li><a href="https://www.pinterest.com/pin/create/button/?url=<?php the_permalink();?>&title=<?php the_title(); ?>" title="Pin it on Pinterest" class="icon-pinterest"><i class="fa fa-pinterest fa-fw"></i></a></li>
+                                <li><a href="https://www.pinterest.com/pin/create/button/?url=<?php the_permalink();?>&title=<?php the_title(); ?>" title="Pin it on Pinterest" class="icon-pinterest"><i class="fa fa-1x fa-pinterest fa-fw"></i></a></li>
                                 
                                 <li><a href="<?php the_permalink();?>feed/" title="RSS" class="icon-rss"><i class="fa fa-rss fa-fw"></i></a></li>
                                 
-                                <li><a href="javascript:window.print()" class="icon-prntr"><i class="fa fa-fw fa-print"></i></a></li>
+                                <li><a href="javascript:window.print()" class="icon-prntr"><i class="fa fa-1x fa-fw fa-print"></i></a></li>
                                 
-                                <li><a href="mailto:?subject=<?php the_title(); ?>&body=Read it from here - <?php the_permalink(); ?>" title="Email This" class="icon-mail"><i class="fa fa-fw fa-envelope"></i></a></li>
+                                <li><a href="mailto:?subject=<?php the_title(); ?>&body=Read it from here - <?php the_permalink(); ?>" title="Email This" class="icon-mail"><i class="fa fa-1x fa-fw fa-envelope"></i></a></li>
                                 
                             </ul><hr><br>
                             
@@ -86,7 +86,9 @@ get_header('main'); ?>
                         <hr>
                         <?php the_tags( '<ul class="taglist"><i class="fa fa-fw fa-tags"></i>  Tags: &nbsp;<li>', '</li><li>', '</li></ul>' ); ?>
                         <br>
-                        <button id="commentbox-btn" type="button" class="btn btn-primary aligncenter"><i class="fa fa-fw fa-comments"></i>&nbsp;Show Comments</button>
+                        <button id="commentbox-btn-show" type="button" class="btn btn-primary aligncenter" <?php echo get_theme_mod('fbcomment_disabler'); ?>><i class="fa fa-fw fa-comments-o"></i>&nbsp;Show Comments</button>
+                        <button id="commentbox-btn-hide" type="button" class="btn btn-primary aligncenter"><i class="fa fa-fw fa-comments-o"></i>&nbsp;Hide Comments</button>
+                        <br>
                         <div id="fbcommentbox">
                             <div class="fb-comments" data-href="<?php the_permalink();?>" data-width="100%" data-numposts="5"></div>
                         </div>
