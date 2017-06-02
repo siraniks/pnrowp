c3b882c0ea206599694284c0f4f1be6a31818170
siraniks <nashayexa@gmail.com>
2017-06-02 17:15:02 +0800

    Tweak: Add a proper 404 page

--- 404.php
+++ 404.php
@@ -1,6 +1,6 @@
 <?php
 /** 
- * The single template file_
+ * 404 Page
  *
  * @package Wordpress
  * @subpackage PENROWP
@@ -8,9 +8,10 @@
  *
  */
 
-get_header('main'); ?>
+get_header('404'); ?>
 <main>
-        
+<!--
+    <br>
     <div class="container">
         <div class="row">
             <div class="card card-block">
@@ -18,5 +19,23 @@ get_header('main'); ?>
             </div>
         </div>
     </div>
-        
-<?php get_footer(); ?>
+-->
+    
+    <div class="container">
+        <div class="row">
+            <div class="mx-auto">
+                <div class="div404 aligncenter">
+                    
+                    <div class="card card-block" style="background-color: rgba(255,255,255, 0.8);">
+                        <img src="<?php echo get_template_directory_uri(); ?>/images/404-pic.png" class="img-fluid" style="height:250px;"/>
+                        <hr>
+                        <p>The page you're looking for was not found.</p>
+                        <a id="btn" href="<?php echo home_url(); ?>" class="btn btn-primary"><i class="fa fa-fw fa-home"></i>&nbsp;Go Home</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+</main>
+<?php get_footer('404'); ?>
