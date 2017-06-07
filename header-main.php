6042b9cee74c224ab2cd2e350d488294f76e4095
siraniks <nashayexa@gmail.com>
2017-06-08 04:26:49 +0800

    Tweak: Change some codes for Facebook SDK

--- header-main.php
+++ header-main.php
@@ -38,13 +38,23 @@
         
         <!--  FB JS SDK -->
         <div id="fb-root"></div>
-        <script>(function(d, s, id) {
-          var js, fjs = d.getElementsByTagName(s)[0];
-          if (d.getElementById(id)) return;
-          js = d.createElement(s); js.id = id;
-          js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.9";
-          fjs.parentNode.insertBefore(js, fjs);
-        }(document, 'script', 'facebook-jssdk'));
+        <script>
+              window.fbAsyncInit = function() {
+                FB.init({
+                  appId      : '<?php echo get_theme_mod('fb-appid'); ?>',
+                  xfbml      : true,
+                  version    : 'v2.9'
+                });
+                FB.AppEvents.logPageView();
+              };
+
+              (function(d, s, id){
+                 var js, fjs = d.getElementsByTagName(s)[0];
+                 if (d.getElementById(id)) {return;}
+                 js = d.createElement(s); js.id = id;
+                 js.src = "//connect.facebook.net/en_US/sdk.js";
+                 fjs.parentNode.insertBefore(js, fjs);
+               }(document, 'script', 'facebook-jssdk'));
         </script>
         
         <header>
